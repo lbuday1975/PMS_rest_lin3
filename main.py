6d7b53781b6a9333639d976e91456af201739b72
lbuday1975 <lbuday@mac.com>
2021-12-28 23:54:39 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+lv_odbc_conn = pyodbc.connect('Driver={/opt/sdb/MaxDB/lib/libsdbodbc.so}'
+                          'Server=lnwts1;'
+                          'Database=PMS;'
+                          'UID=BWB_ADM;'
+                          'PWD=BALETT')
+
+lv_cursor = lv_odbc_conn.cursor()
+lv_cursor.execute('SELECT * FROM SYS_DEF')
+for lv_item in lv_cursor:
+    print('TEST:' + lv_item[2] + ' ' + lv_item[3] )
+
+lv_cursor.close()
+lv_odbc_conn.close()
