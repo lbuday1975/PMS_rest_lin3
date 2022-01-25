08bb3e587ba1660e7a2781b7f0511ca5822b628a
lbuday1975 <lbuday@mac.com>
2022-01-25 22:20:23 +0100

    Initial commit2

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import pyodbc, flask
+import flask
+import pms_db
 
-lv_odbc_conn = pyodbc.connect('Driver={/opt/sdb/MaxDB/lib/libsdbodbc.so}'
-                          'Server=lnwts1;'
-                          'Database=PMS;'
-                          'UID=BWB_ADM;'
-                          'PWD=BALETT')
 
-lv_cursor = lv_odbc_conn.cursor()
-lv_cursor.execute('SELECT * FROM SYS_DEF')
-for lv_item in lv_cursor:
-    print('TEST:' + lv_item[2] + ' ' + lv_item[3] )
+pms_db.get_syslist()
 
-lv_cursor.close()
-lv_odbc_conn.close()
