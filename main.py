6808599bd8723b353f6994a3b9ef1a2f0380677d
lbuday1975 <lbuday@mac.com>
2022-01-25 22:44:18 +0100

    pms_command.def

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
-import pms_db
+import pms_db, pms_command
 
+# lv_rc = pms_command.chk_connect()
+print("RET: " + pms_command.get_sys_var('DB8', '\<DEF_HOST\>'))
 
-pms_db.get_syslist()
-
+# pms_db.get_syslist()
