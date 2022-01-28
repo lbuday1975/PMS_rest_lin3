8826e88bf16afb35d7f7aee69a8d15439ef3aaa7
lbuday1975 <lbuday@mac.com>
2022-01-28 22:18:50 +0100

    pms_command.set + get var OK

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import pms_db, pms_command
 
 # lv_rc = pms_command.chk_connect()
 print("RET: " + pms_command.get_sys_var('DB8', '\<DEF_HOST\>'))
-
+print("Change to proc rc:" + str(pms_command.set_sys_var('DB8', '\<DEF_HOST\>', 'p1-lnw_OK')))
+print("RET: " + pms_command.get_sys_var('DB8', '\<DEF_HOST\>'))
 # pms_db.get_syslist()
--- pms_command.py
+++ pms_command.py
@@ -5,7 +5,7 @@ import subprocess as sp
 # Check connection to PMS server
 def chk_connect():
     lv_cmd = '/opt/PMS/bin/pms_commander_server.sh -c SYSTEM_LIST'
-    lv_rc = -1
+    lv_rc = 999
 
     lv_proc = sp.Popen([lv_cmd], stdout=sp.PIPE, shell=True)
     try:
@@ -13,7 +13,7 @@ def chk_connect():
         lv_rc = lv_proc.poll()
     except sp.CalledProcessError as lv_ex:
         print("ERROR: " + lv_ex.output.decode())
-        lv_rc = 9
+        lv_rc = 999
 
     return lv_rc
 
@@ -21,16 +21,31 @@ def chk_connect():
 def get_sys_var(la_sys_name, la_sys_var):
     lv_cmd = f'/opt/PMS/bin/pms_commander_server.sh -c GET_VARIABLE -SYS_NAME {la_sys_name} -VAR_NAME {la_sys_var}'
     lv_proc = sp.Popen([lv_cmd], stdout=sp.PIPE, shell=True)
+    lv_rc = "ERROR"
     try:
         lv_out, lv_err = lv_proc.communicate()
         lv_rc = str(lv_proc.poll())
     except sp.CalledProcessError as lv_ex:
         print("ERROR: " + lv_ex.output.decode())
-        lv_rc = "9"
+        lv_rc = "ERROR"
 
     for lv_line in lv_out.splitlines():
         if re.search("VALUE", lv_line.decode()):
             print(str(lv_line.decode().split(':')))
             lv_rc = str(lv_line.decode().split(':')[1])
+
+    return lv_rc
+
+
+def set_sys_var(la_sys_name, la_sys_var, la_value):
+    lv_rc = 999
+    lv_cmd = f'/opt/PMS/bin/pms_commander_server.sh -c SET_VARIABLE -SYS_NAME {la_sys_name} -VAR_NAME {la_sys_var} -VALUE {la_value}'
+    lv_proc = sp.Popen([lv_cmd], stdout=sp.PIPE, shell=True)
+    try:
+        lv_out, lv_err = lv_proc.communicate()
+        lv_rc = str(lv_proc.poll())
+    except sp.CalledProcessError as lv_ex:
+        print("ERROR: " + lv_ex.output.decode())
+        lv_rc = 999
 
     return lv_rc
