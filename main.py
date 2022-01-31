a12c51e95402f69b285168589a8aab19290862a5
lbuday1975 <lbuday@mac.com>
2022-01-31 12:12:55 +0100

    pms_command.set + get var OK

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask
-import marshmallow, jsonify
+from flask import Flask, request
+
+import marshmallow, jsonify, pms_command
 from flask_httpauth import HTTPBasicAuth
+
 auth = HTTPBasicAuth()
+
 
 @auth.get_password
 def get_password(username):
@@ -9,9 +12,11 @@ def get_password(username):
         return 'python'
     return None
 
+
 @auth.error_handler
 def unauthorized():
     return {'error': 'Unauthorized access'}
+
 
 # lv_rc = pms_command.chk_connect()
 # print("RET: " + pms_command.get_sys_var('DB8', '\<DEF_HOST\>'))
@@ -22,11 +27,51 @@ def unauthorized():
 app = Flask(__name__)
 
 
-@app.route("/get_var")
+@app.route("/api/get_var", methods=['POST', 'GET'])
 @auth.login_required
 def get_var():
+    lv_grc = ""
+    if request.method == 'POST':
+        ls_sys_name = request.args.get('sys_name')
+        ls_var_name = request.args.get('var_name')
+        lv_rc = pms_command.chk_connect()
+        if lv_rc == 0:
+            print(f'ACT: get variable value to {ls_sys_name}-{ls_var_name}')
+            print("--> RET: " + pms_command.get_sys_var(f'{ls_sys_name}', f'\<{ls_var_name}\>'))
+            lv_grc = 0
+        else:
+            print("PMS server connection ERROR")
+            lv_grc = "PMS server connection ERROR"
+
+    return str(lv_grc)
+
+
+@app.route("/api/start_cmd", methods=['POST'])
+@auth.login_required
+def api_start_cmd():
     print("kokokokk")
     return "Adom maar...."
+
+
+@app.route("/api/set_var", methods=['POST'])
+@auth.login_required
+def api_set_var():
+    lv_grc = -1
+    if request.method == 'POST':
+        ls_sys_name = request.args.get('sys_name')
+        ls_var_name = request.args.get('var_name')
+        ls_var_value = request.args.get('var_value')
+        lv_rc = pms_command.chk_connect()
+        if lv_rc == 0:
+            print(f'Act: set variable value to {ls_sys_name}-{ls_var_name} value: {ls_var_value}')
+            print("--> RC : " + str(pms_command.set_sys_var(f'{ls_sys_name}', f'\<{ls_var_name}\>', f'{ls_var_value}')))
+            print("--> RET: " + pms_command.get_sys_var(f'{ls_sys_name}', f'\<{ls_var_name}\>'))
+            lv_grc = 0
+        else:
+            print("PMS server connection ERROR")
+            lv_grc = "PMS server connection ERROR"
+
+    return str(lv_grc)
 
 
 if __name__ == "__main__":
--- pms_command.py
+++ pms_command.py
@@ -1,5 +1,8 @@
+import logging
 import re
 import subprocess as sp
+
+DEBUG = False
 
 
 # Check connection to PMS server
@@ -27,11 +30,14 @@ def get_sys_var(la_sys_name, la_sys_var):
         lv_rc = str(lv_proc.poll())
     except sp.CalledProcessError as lv_ex:
         print("ERROR: " + lv_ex.output.decode())
+        lv_out = ""
         lv_rc = "ERROR"
 
     for lv_line in lv_out.splitlines():
         if re.search("VALUE", lv_line.decode()):
-            print(str(lv_line.decode().split(':')))
+            if DEBUG:
+                print("DEBUG = " + str(logging.DEBUG))
+                print(str(lv_line.decode().split(':')))
             lv_rc = str(lv_line.decode().split(':')[1])
 
     return lv_rc
@@ -40,6 +46,20 @@ def get_sys_var(la_sys_name, la_sys_var):
 def set_sys_var(la_sys_name, la_sys_var, la_value):
     lv_rc = 999
     lv_cmd = f'/opt/PMS/bin/pms_commander_server.sh -c SET_VARIABLE -SYS_NAME {la_sys_name} -VAR_NAME {la_sys_var} -VALUE {la_value}'
+    lv_proc = sp.Popen([lv_cmd], stdout=sp.PIPE, shell=True)
+    try:
+        lv_out, lv_err = lv_proc.communicate()
+        lv_rc = str(lv_proc.poll())
+    except sp.CalledProcessError as lv_ex:
+        print("ERROR: " + lv_ex.output.decode())
+        lv_rc = 999
+
+    return lv_rc
+
+
+def start_cmd(la_sys_name, la_proc_name, la_param):
+    lv_rc = 999
+    lv_cmd = f'/opt/PMS/bin/pms_commander_server.sh -c START_COMMAND -SYS_NAME {la_sys_name} -VAR_NAME {la_sys_var} -VALUE {la_value}'
     lv_proc = sp.Popen([lv_cmd], stdout=sp.PIPE, shell=True)
     try:
         lv_out, lv_err = lv_proc.communicate()
